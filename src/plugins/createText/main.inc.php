b8312f306b2cbd555a28ba2959ee11a1dab0aa55
Justin <xbljustin@hotmail.com>
2017-03-01 15:53:37 -0500

    Update main.inc.php

--- src/plugins/createText/main.inc.php
+++ src/plugins/createText/main.inc.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Plugin Name: skeleton
+Plugin Name: craeteText
 Version: 2.7.0
-Description: This is not a plugin. It's a skeleton for future plugins.
+Description: This is not a plugin. It's a createText for future plugins.
 Plugin URI: http://piwigo.org/ext/extension_view.php?eid=543
 Author: Mistic
 Author URI: http://www.strangeplanet.fr
@@ -22,12 +22,12 @@ defined('PHPWG_ROOT_PATH') or die('Hacking attempt!');
 // +-----------------------------------------------------------------------+
 global $prefixeTable;
 
-define('SKELETON_ID',      basename(dirname(__FILE__)));
-define('SKELETON_PATH' ,   PHPWG_PLUGINS_PATH . SKELETON_ID . '/');
-define('SKELETON_TABLE',   $prefixeTable . 'skeleton');
-define('SKELETON_ADMIN',   get_root_url() . 'admin.php?page=plugin-' . SKELETON_ID);
-define('SKELETON_PUBLIC',  get_absolute_root_url() . make_index_url(array('section' => 'skeleton')) . '/');
-define('SKELETON_DIR',     PHPWG_ROOT_PATH . PWG_LOCAL_DIR . 'skeleton/');
+define('CREATE_TEXT_ID',      basename(dirname(__FILE__)));
+define('CREATE_TEXT_PATH' ,   PHPWG_PLUGINS_PATH . CREATE_TEXT_ID . '/');
+define('CREATE_TEXT_TABLE',   $prefixeTable . 'createText');
+define('CREATE_TEXT_ADMIN',   get_root_url() . 'admin.php?page=plugin-' . CREATE_TEXT_ID);
+define('CREATE_TEXTN_PUBLIC',  get_absolute_root_url() . make_index_url(array('section' => 'createText')) . '/');
+define('CREATE_TEXT_DIR',     PHPWG_ROOT_PATH . PWG_LOCAL_DIR . 'createText/');
 
 
 
@@ -35,7 +35,7 @@ define('SKELETON_DIR',     PHPWG_ROOT_PATH . PWG_LOCAL_DIR . 'skeleton/');
 // | Add event handlers                                                    |
 // +-----------------------------------------------------------------------+
 // init the plugin
-add_event_handler('init', 'skeleton_init');
+add_event_handler('init', 'create_text_init');
 
 /*
  * this is the common way to define event functions: create a new function for each event you want to handle
@@ -43,20 +43,20 @@ add_event_handler('init', 'skeleton_init');
 if (defined('IN_ADMIN'))
 {
   // file containing all admin handlers functions
-  $admin_file = SKELETON_PATH . 'include/admin_events.inc.php';
+  $admin_file = CREATE_TEXT_PATH . 'include/admin_events.inc.php';
 
   // new tab on photos-add page
-  add_event_handler('tabsheet_before_select', 'skeleton_tabsheet_before_select_add',
+  add_event_handler('tabsheet_before_select', 'create_text_tabsheet_before_select_add',
     EVENT_HANDLER_PRIORITY_NEUTRAL, $admin_file);
 }
 
-function skeleton_init()
+function create_text_init()
 {
   global $conf;
 
   // load plugin language file
-  load_language('plugin.lang', SKELETON_PATH);
+  load_language('plugin.lang', CREATE_TEXT_PATH);
 
   // prepare plugin configuration
-  $conf['skeleton'] = safe_unserialize($conf['skeleton']);
+  $conf['createText'] = safe_unserialize($conf['createText']);
 }

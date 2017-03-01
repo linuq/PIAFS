e435836e9b9f628bfa9657915e8a1742e2bb853e
Justin <xbljustin@hotmail.com>
2017-03-01 17:28:36 -0500

    Update main.inc.php

--- src/plugins/createText/main.inc.php
+++ src/plugins/createText/main.inc.php
@@ -56,7 +56,4 @@ function create_text_init()
 
   // load plugin language file
   load_language('plugin.lang', CREATE_TEXT_PATH);
-
-  // prepare plugin configuration
-  $conf['createText'] = safe_unserialize($conf['createText']);
 }

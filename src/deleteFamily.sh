243ef3eb5b5fa5ac47051279bcef12d638c43b4b
Justin <xbljustin@hotmail.com>
2017-03-07 14:31:27 -0500

    Update deleteFamily.sh

--- src/deleteFamily.sh
+++ src/deleteFamily.sh
@@ -1,42 +1,33 @@
 #!/bin/bash
 
+cd '/var/www/html/src'
+familyName="$1"
 
-function delete_family
-{
-        echo "Quel est le nom de la famille a supprimer?"
-        read familyName
+if [ $# -eq 0 ]
+  then
+	echo "Quel est le nom de la famille a supprimer?"
+	read familyName
+fi
 
-        echo
+echo
 
-        cd '/var/www/html/'
+cd '/var/www/html/src'
 
-        if [ -d $familyName ]; then
-           chmod -R 222 $familyName
-           rm -rf $familyName
-           echo "La famille $familyName a été supprimée."
-        else
-           echo "La famille $familyName n'existe pas."
-        fi
+if [ -d "$familyName" ]; then
+   chmod -R 222 $familyName
+   rm -rf $familyName
+   echo "La famille $familyName a été supprimée."
+else
+   echo "La famille $familyName n'existe pas."
+   exit 0
+fi
 
-        cd '/etc/apache2/'
-        sed -i "s#Alias /$familyName /var/www/html##g" apache2.conf
+cd '/etc/apache2/'
+sed -i "s#Alias /$familyName /var/www/html##g" apache2.conf
 
-        cd '/etc/apache2/sites-available/'
-        sed -i "s#Alias /$familyName /var/www/html##g" 000-default.conf
-        echo "La famille a été supprimé des fichiers de configurations"
+cd '/etc/apache2/sites-available/'
+sed -i "s#Alias /$familyName /var/www/html##g" 000-default.conf
+echo "La famille a été supprimé des fichiers de configurations"
 
-        eval "/etc/init.d/apache2 reload"
-}
+eval "/etc/init.d/apache2 reload"
 
-delete_family
-
-while true;
-do
-    read -r -p "Voulez vous supprimer un autre famille? oui/non : " response
-    if [[ $response =~ ^([oO][uU][iI]|[oO])$ ]]
-    then
-        delete_family
-    else
-        exit 0
-    fi
-done

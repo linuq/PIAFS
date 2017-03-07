175949189b776a637c3f83e0854124d5942e8de2
Justin <xbljustin@hotmail.com>
2017-03-07 13:31:46 -0500

    Update createFamily.sh

--- src/createFamily.sh
+++ src/createFamily.sh
@@ -1,46 +1,35 @@
 #!/bin/bash
 
-function create_family
-{
-        echo "Quel est le nom de la famille a créer?"
-        read familyName
+cd '/var/www/html/src'
+familyName="$1"
 
-        cd '/var/www/html/'
-        mkdir $familyName
+if [ $# -eq 0 ]
+  then
+	echo "Quel est le nom de la famille a ajouter?"
+	read familyName
+fi
+cd '/var/www/html/src'
+mkdir $familyName
 
-        echo "La famille $familyName a été créée"
+echo "La famille $familyName a été créée"
 
-        echo
+echo
 
-        cd $familyName
-        mkdir -p local/config _data upload
-        echo "Création des fichiers nécessaires"
+cd $familyName
+mkdir -p local/config _data upload
+echo "Création des fichiers nécessaires"
 
-        echo
+echo
 
-        chown -R www-data:www-data .
+chown -R www-data:www-data .
 
-        cd '/etc/apache2/'
-        echo "Alias /$familyName /var/www/html" >> apache2.conf
+cd '/etc/apache2/'
+echo "Alias /$familyName /var/www/html" >> apache2.conf
 
-        cd '/etc/apache2/sites-available/'
-        add="   Alias /$familyName /var/www/html"
+cd '/etc/apache2/sites-available/'
+add="   Alias /$familyName /var/www/html"
 
-        sed -i "13i\ $add" 000-default.conf
-        echo "Ajout de la famille dans la configuration de Apache"
+sed -i "13i\ $add" 000-default.conf
+echo "Ajout de la famille dans la configuration de Apache"
 
-        eval "/etc/init.d/apache2 reload"
-}
-
-create_family
-
-while true;
-do
-    read -r -p "Voulez vous créer un autre famille? oui/non :" response
-    if [[ $response =~ ^([oO][uU][iI]|[oO])$ ]]
-    then
-        create_family
-    else
-        exit 0
-    fi
-done
+eval "/etc/init.d/apache2 reload"

b224c2bbd94c088872c238530610eec592819fec
Justin <xbljustin@hotmail.com>
2017-03-07 11:34:51 -0500

    Added loop

--- src/createFamily.sh
+++ src/createFamily.sh
@@ -1,28 +1,46 @@
 #!/bin/bash
-echo "Quel est le nom de la famille à créer?"
-read familyName
 
-echo
+function create_family
+{
+        echo "Quel est le nom de la famille �|  créer?"
+        read familyName
 
-mkdir $familyName
-echo "La famille $familyName à été créée"
+        cd '/var/www/html/'
+        mkdir $familyName
 
-echo
+        echo "La famille $familyName �|  été créée"
 
-cd $familyName
-mkdir -p local/config _data upload
-echo "Création des fichiers nécessaires"
+        echo
 
-echo
+        cd $familyName
+        mkdir -p local/config _data upload
+        echo "Création des fichiers nécessaires"
 
-chown -R www-data:www-data .
+        echo
 
-cd '/etc/apache2/'
-echo "Alias /$familyName /var/www/html" >> apache2.conf	
+        chown -R www-data:www-data .
 
-cd '/etc/apache2/sites-available/'
-add="	Alias /$familyName /var/www/html"
-sed -i "13i\ $add" 000-default.conf
-echo "Ajout de l'alias dans la configuration de Apache"
+        cd '/etc/apache2/'
+        echo "Alias /$familyName /var/www/html" >> apache2.conf
 
-eval "/etc/init.d/apache2 reload"
+        cd '/etc/apache2/sites-available/'
+        add="   Alias /$familyName /var/www/html"
+
+        sed -i "13i\ $add" 000-default.conf
+        echo "Ajout de l'alias dans la configuration de Apache"
+
+        eval "/etc/init.d/apache2 reload"
+}
+
+create_family
+
+while true;
+do
+    read -r -p "Voulez vous créer un autre famille? oui/non :" response
+    if [[ $response =~ ^([oO][uU][iI]|[oO])$ ]]
+    then
+        create_family
+    else
+        exit 0
+    fi
+done

32624e8827a3ea49e89535271c93a173e1f53ce9
Justin <xbljustin@hotmail.com>
2017-03-08 09:49:41 -0500

    Update create_family_tests.php

--- tests/create_family_tests.php
+++ tests/create_family_tests.php
@@ -1,21 +1,21 @@
 <?php
 
-include_once './src/createFamily.sh';
+include_once '../src/createFamily.sh';
 
 class create_family_tests extends PHPUnit_Framework_TestCase
 {
 
     public function test_create_a_new_family()
     {
-	$createFamily=shell_exec('./src/createFamily.sh var4');
-	$doFileExist=file_exists('./src/var4');
+	$createFamily=shell_exec('../src/createFamily.sh var4');
+	$doFileExist=file_exists('../src/var4');
 
 	$this->assertTrue($doFileExist);
     }
 
     public function test_attempt_to_create_family_that_already_exists()
     {
-	$output=shell_exec('./src/createFamily.sh var4');
+	$output=shell_exec('../src/createFamily.sh var4');
 	$expected="La famille var4 existe déjà\n";
 
 	$this->assertEquals($expected,$output);
